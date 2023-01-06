cf2f6ade44585ff60f827ecfce0c41c26615dcd6
dadiallo <d.abdoulayead28@gmail.com>
2023-01-06 21:11:34 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# springbootTp1" 
---
+++ src/main/java/com/springbootTp1/seance1/Seance1Application.java
@@ -0,0 +1,13 @@
+package com.springbootTp1.seance1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seance1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seance1Application.class, args);
+	}
+
+}
